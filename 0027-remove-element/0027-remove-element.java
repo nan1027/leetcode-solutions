9adc736f2ce8df5b185c4cfe6287a61612c12468
Nandita Rishishwar <nanditar1027@gmail.com>
2026-07-08 10:41:51 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.16%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int insertPos=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[insertPos]=nums[i];
+                insertPos++;
+            }
+        }
+        return insertPos;
+    }
+}
